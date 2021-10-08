0e6cd852db2390ca4baee3c88fd96d86f705d963
Rahul Singh Shekhawat <rahulsshekhawat5@gmail.com>
2021-10-09 01:41:45 +0530

    completed date picker comp

--- src/Components/DatePickerComp.jsx
+++ src/Components/DatePickerComp.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useState, forwardRef } from "react";
 import { BsCalendar4 } from "react-icons/bs";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const DatePickerComp = ({ category }) => {
   const [date, updateDate] = useState(null);
+  const CustomInput = forwardRef(({ value, onClick }, ref) => (
+    <div className="custom-input">
+      <p ref={ref}>{value ? value : "DD/MM/YYY"}</p>
+      <BsCalendar4 className="calender-icon" onClick={onClick} />
+    </div>
+  ));
   return (
     <div className="date-picker-wrapper">
       <p>{category}</p>
@@ -12,11 +18,8 @@ const DatePickerComp = ({ category }) => {
         <DatePicker
           selected={date}
           onChange={(newDate) => updateDate(newDate)}
-          placeholderText="DD/MM/YYY"
-          isClearable
+          customInput={<CustomInput />}
         />
-
-        <BsCalendar4 />
       </div>
     </div>
   );
